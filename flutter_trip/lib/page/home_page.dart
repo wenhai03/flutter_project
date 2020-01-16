9b55a87e7f85ed4fabfdfd152115299f924e5230
xiewenhai <810068733@qq.com>
2020-01-16 15:58:23 +0800

    提交两个localNavList和gridNavModel

--- flutter_trip/lib/page/home_page.dart
+++ flutter_trip/lib/page/home_page.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_trip/dao/home_dao.dart';
+import 'package:flutter_trip/model/common_model.dart';
+import 'package:flutter_trip/model/grid_nav_model.dart';
+import 'package:flutter_trip/model/home_model.dart';
+import 'package:flutter_trip/widget/grid_nav.dart';
+import 'package:flutter_trip/widget/local_nav.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 const SEARCH_BAR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
@@ -16,6 +24,43 @@ class _HomePageState extends State<HomePage> {
     'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
   ];
   double appBarAlpha = 0;
+  String resultString = '';
+  List<CommonModel> localNavList = [];
+  GridNavModel gridNavModel;
+
+//  loadData(){
+//    HomeDao.fetch().then((result){
+//      setState(() {
+//        resultString = json.encode(result);
+//      });
+//    }).catchError((e){
+//      resultString = e.toString();
+//    });
+//  }
+
+  loadData () async{
+
+    try{
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        localNavList = model.localNavList;
+        gridNavModel = model.gridNav;
+      });
+    }catch(e){
+      setState(() {
+//        resultString = e.toString();
+        print(e);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
 
   _onScroll(offset) {
     double alpha = offset / APPBAR_SCROLL_OFFSET;
@@ -35,56 +80,69 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          MediaQuery.removePadding(
-            removeTop: true,
-            context: context,
-            child: NotificationListener(
-              onNotification: (scrollNotification) {
-                if (scrollNotification is ScrollUpdateNotification &&
-                    scrollNotification.depth == 0) {
-                  //滚动且是列表滚动的时候
-                  _onScroll(scrollNotification.metrics.pixels);
-                }
-              },
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    height: 160,
-                    child: Swiper(
-                      itemCount: _imageUrls.length,
-                      autoplay: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Image.network(
-                          _imageUrls[index],
-                          fit: BoxFit.fill,
-                        );
-                      },
-                      pagination: SwiperPagination(),
-                    ),
+          Stack(
+            children: <Widget>[
+              MediaQuery.removePadding(
+                // 移除ListView距离屏幕顶部的边距
+                removeTop: true,
+                context: context,
+                child: NotificationListener(
+                  // 可以监听所有列表的滚动的widget
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+                      //滚动且是列表滚动的时候，如果是轮播滚动 不做处理
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                  },
+                  child: ListView(
+                    children: <Widget>[
+                      Container(
+                        height: 160,
+                        child: Swiper(
+                          itemCount: _imageUrls.length,
+                          autoplay: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Image.network(
+                              _imageUrls[index],
+                              fit: BoxFit.fill,
+                            );
+                          },
+                          pagination: SwiperPagination(),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                        child: LocalNav(localNavList: localNavList),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                        child: GridNav(gridNavModel: gridNavModel)
+                      ),
+                      Container(
+                        height: 800,
+                        child: ListTile(
+                          title: Text('resultString'),
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    height: 800,
-                    child: ListTile(
-                      title: Text('哈哈111'),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Opacity(
-            opacity: appBarAlpha,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text('首页'),
                 ),
               ),
-            ),
-          )
+              Opacity(
+                opacity: appBarAlpha,
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text('首页'),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
